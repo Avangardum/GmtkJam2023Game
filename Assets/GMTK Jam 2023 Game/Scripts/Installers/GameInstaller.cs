18a74798736f771b686a454b26970471010e6621
Avangardum <yurykuz65@gmail.com>
2023-07-07 19:36:20 +0300

    Made the Game scene and GameInstaller

---
+++ Assets/GMTK Jam 2023 Game/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Avangardum.GmtkJam2023Game.Installers
+{
+    public class GameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
